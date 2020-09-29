9ae5b0e550f7ccd169be3741577e75998982ec42
Masatoshi <msts.oo0131@gmail.com>
2020-09-29 14:24:37 +0900

    item_listing_model_making

---
+++ db/migrate/20200929051127_create_items.rb
@@ -0,0 +1,15 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string  :name
+      t.integer
+      t.integer
+      t.integer
+      t.integer
+
+
+
+      t.timestamps
+    end
+  end
+end
