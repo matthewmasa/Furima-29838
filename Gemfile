b02c9d4671ab7d1b2f83311cb439081da1149ed7
Masatoshi <msts.oo0131@gmail.com>
2020-09-29 19:42:59 +0900

    rspec_remodify

--- Gemfile
+++ Gemfile
@@ -60,16 +60,14 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'active_hash', '~> 3.1'
 gem 'devise', '~> 4.7', '>= 4.7.3'
-<<<<<<< HEAD
 gem 'dotenv-rails', '~> 2.7', '>= 2.7.6'
 gem 'rails-erd', '~> 1.6'
 gem 'mini_magick', '~> 4.10', '>= 4.10.1'
 gem 'ruby-graphviz', '~> 1.2', '>= 1.2.5'
+gem 'rb-readline', '~> 0.5.5'
 
 group :production do
   gem 'rails_12factor'
 end
-=======
 
->>>>>>> 57fcb31528fc62010ad6f865970bd2564af74bbc
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,19 +20,32 @@ RSpec.describe User, type: :model do
      expect(user.errors[:nickname]).to be_invalid
      end
 
-     it 'is invalid' do
-       user.email= "test.com"
+     it "nicknameがない状態" do
+       user.nickname = nil
        user.valid?
+       expect(user.errors.full_messages).to include("Nickname can't be blank")
+     end
+
+     it "is invalid if email is not uniqueness " do
+       user = user.dup
+       user.email = @user.email.upcase
+       user.invalid?
        expect(user.errors[:email]).to be_invalid
      end
 
-     it 'is invalid' do #パスワードがない状態#
+     it "emailがない時" do
+       user.email = nil
+       user.valid?
+       expect(user.errors.full_messages).to include("Email can't be blank")
+     end
+
+     it 'パスワードがない状態' do
        user.password= nil
        user.valid?
        expect(user.errors[:password]).to be_invalid
      end
 
-     it 'is invalid' do #半角英数での登録じゃない状態#
+     it '半角英数での登録じゃない状態' do
        user.password= "123456"
        user.valid?
        expect(user.eroors[:password]).to be_invalid
@@ -44,31 +57,31 @@ RSpec.describe User, type: :model do
        expect(user.errors[:password]). to be_invalid
      end
 
-     it 'is invalid' do #パスワードが5文字以下の場合#
+     it 'パスワードが5文字以下の場合' do
        user.password= "a1234"
        user.valid?
        expect(user.errors[:password]).to be_invalid
      end
 
-     it 'is invalid' do   #苗字がない場合#
+     it '苗字がない場合' do
        user.family_name=nil
        user.valid?
        expect(user.errors[:family_name]).to be_invalid
      end
 
-     it 'is invalid' do #名前がない場合#
+     it '名前がない場合' do
        user.first_name=nil
        user.invalid?
        expect(user.errors[:first_name]). to be_invalid
      end
 
-     it 'is invalid' do  #苗字のカナがない場合#
+     it '苗字のカナがない場合' do
        user.family_name_kana=nil
        user.valid?
        expect(user.errors[:family_name_kana]). to be_invalid
      end
 
-     it 'is invalid' do  #名前のカナがない場合#
+     it '名前のカナがない場合' do
        user.first_name_kana=nil
        user.invalid?
        expect(user.errors[:first_name_kana]). to be_invalid
@@ -86,7 +99,7 @@ RSpec.describe User, type: :model do
        expect(user.errors[:family_name_kana]). to be_invalid
      end
 
-     it 'is invalid' do #誕生日がない時#
+     it '誕生日がない時' do
        user.birth_day=nil
        user.valid?
        expect(user.errors{:birth_day}). to be_invalid
