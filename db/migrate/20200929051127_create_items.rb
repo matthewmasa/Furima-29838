eed06817c8a893c42dc9e36d4001c2055f2f6d4e
Masatoshi <msts.oo0131@gmail.com>
2020-09-30 20:06:01 +0900

    modelとcontrollerの作成

--- db/migrate/20200929051127_create_items.rb
+++ db/migrate/20200929051127_create_items.rb
@@ -2,11 +2,14 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
       t.string  :name
-      t.integer
-      t.integer
-      t.integer
-      t.integer
-
+      t.integer :price
+      t.integer :genre
+      t.integer :delivery_fee_id
+      t.integer :shipping_area_id
+      t.integer :shipping_day_id
+      t.integer :status_id
+      t.text    :content
+      t.references foreign_key: true
 
 
       t.timestamps
